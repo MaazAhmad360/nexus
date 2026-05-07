b034b09478c9ae401a4a88156c6f0ed23902a34b
Maaz Ahmad <muaz.ahmad76@gmail.com>
2026-05-07 22:58:43 +0500

    feat(backend): implement Django REST Framework backend

---
+++ nexus-web/nexus_backend/nexus/urls.py
@@ -0,0 +1,18 @@
+# urls.py
+
+from django.urls import path
+from .auth import auth_router as authapis
+from .homepage import hp_router as homepageapi
+from .posts import post_router as postsapi
+from .story import story_router as storyapi
+from .user import user_router as userapi
+from .messaging import message_router
+
+urlpatterns = [
+    path("auth/", authapis.urls),
+    path("homepage/", homepageapi.urls),
+    path("posts/", postsapi.urls),
+    path("story/", storyapi.urls),
+    path("user/", userapi.urls),
+    path("chat/", message_router.urls),
+]
